83f891709e66997e766fa2a1366a587e7d7e2605
Giannina Baccelliere <gbaccellierem@gmail.com>
2022-09-13 15:55:08 -0300

    Simple Counter listo

---
+++ src/js/icons.js
@@ -0,0 +1,8 @@
+import fontawesome from '@fontawesome/fontawesome';
+import faClock from '@fontawesome/fontawesome-free-solid/faCheck';
+
+fontawesome.config = {
+    autoReplaceSvg: 'nest'
+}
+
+fontawesome.library.add(faClock);
